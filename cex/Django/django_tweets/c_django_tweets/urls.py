5c663185b9ce421531c3e31086830c84d92198f7
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-09-15 11:38:19 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- cex/Django/django_tweets/c_django_tweets/urls.py
+++ cex/Django/django_tweets/c_django_tweets/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from tweet.views import Index, Profile
+from tweet.views import Index, Profile, PostTweet
 
 admin.autodiscover()
 
@@ -23,5 +23,6 @@ urlpatterns = [
   url(r'^$', Index.as_view()),
   url(r'^user/(\w+)/$', Profile.as_view()),  
   url(r'^admin/', include(admin.site.urls)),
+  url(r'^user/(\w+)/post/$', PostTweet.as_view())
 ]
 
