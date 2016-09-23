33ac78cf36176cddd9e15e6ca2bdf589be0836a3
rxl194 <rxl194@users.noreply.github.com>
2016-09-23 16:41:15 -0700

    Update cpcv_install_2.bash

--- proj_setup/bjou_dev/cpcv_install_2.bash
+++ proj_setup/bjou_dev/cpcv_install_2.bash
@@ -11,8 +11,11 @@ cmake -D CMAKE_INSTALL_PREFIX=/opt/ocv2.4.13 \
   -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_FFMPEG=OFF \
   .. |& tee cmake.log
 make |& tee build.log
-make install; ldconfig
+make install
 cp lib/cv2.so /usr/local/lib/python2.7/dist-packages
 cp -p unix-install/opencv.pc /usr/lib/pkgconfig
+checkinstall
+sh -c 'echo "/opt/ocv2.4.13/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
 cd
 
