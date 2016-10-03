48edb4b91511d6b687b5e82c9f46cb4cfda72d03
rxl194 <Rendong_Lin@yahoo.com>
2016-10-03 22:39:59 +0000

    .

---
+++ proj_setup/bjou_dev/cpcv_install_2_ocv3.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p /git; cd /git
+rm -rf  ocv3.1.0
+git clone https://github.com/rxl194/opencv ocv3.1.0
+cd  ocv3.1.0
+rm -rf ubuntu; mkdir -p ubuntu; cd ubuntu
+cmake -D CMAKE_INSTALL_PREFIX=/opt/ocv3.1.0 \
+  -D INSTALL_PYTHON_EXAMPLES=ON \
+  -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON \
+  -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_FFMPEG=OFF \
+  .. |& tee cmake.log
+make |& tee build.log
+make install
+cp lib/cv2.so /usr/local/lib/python2.7/dist-packages
+#cp lib/cv2.so /opt/meanstack-3.2.9-0/miniconda2/lib/python2.7/site-packages
+cp -p unix-install/opencv.pc /usr/lib/pkgconfig
+#checkinstall
+rm /etc/ld.so.conf.d/opencv.conf
+sh -c 'echo "/opt/ocv3.1.0/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
+cd
+
