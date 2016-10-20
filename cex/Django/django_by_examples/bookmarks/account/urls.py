b210cc78cadaa1518a367ecd7b2c0f312c60c3f3
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-10-20 14:16:02 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- cex/Django/django_by_examples/bookmarks/account/urls.py
+++ cex/Django/django_by_examples/bookmarks/account/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'), 
     
     url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.edit, name='edit'),
 
     # login / logout urls
     url(r'^login/$',
