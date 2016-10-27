cf177471ecbc4deb5e6d185eb805cb9f7cced97a
Rendong_Lin <Rendong_Lin@yahoo.com>
2016-10-26 21:09:38 -0700

    Signed-off-by: Rendong_Lin <Rendong_Lin@yahoo.com>

--- cex/Django/django_by_examples/bookmarks/images/serializers.py
+++ cex/Django/django_by_examples/bookmarks/images/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Image
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Image
         fields = ('user', 'image')        
--- cex/Django/django_by_examples/bookmarks/images/views.py
+++ cex/Django/django_by_examples/bookmarks/images/views.py
@@ -130,7 +130,7 @@ class ImageList(APIView):
     def post(self, request, format=None):
         serializer = ImageSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
