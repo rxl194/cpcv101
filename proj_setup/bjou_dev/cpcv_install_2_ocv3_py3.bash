051154b20f13593358cfe91d8d08b3da560c2a64
rxl194 <Rendong_Lin@yahoo.com>
2016-10-31 23:07:12 +0000

    .

--- proj_setup/bjou_dev/cpcv_install_2_ocv3_py3.bash
+++ proj_setup/bjou_dev/cpcv_install_2_ocv3_py3.bash
@@ -11,18 +11,18 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D INSTALL_C_EXAMPLES=ON \
   -D INSTALL_PYTHON_EXAMPLES=ON \
   -D OPENCV_EXTRA_MODULES_PATH=~/git/ocv3.1.0_contrib/modules \
-  -D BUILD_EXAMPLES=ON \
   -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON \
   -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON -D WITH_FFMPEG=OFF \
   .. |& tee cmake.log
 make |& tee build.log
-#make install
-#cp lib/cv2.so /usr/local/lib/python2.7/dist-packages
+make install |& tee install.log
+cp lib/cv2.so /usr/local/lib/python2.7/dist-packages
 #cp lib/cv2.so /opt/meanstack-3.2.9-0/miniconda2/lib/python2.7/site-packages
-#cp -p unix-install/opencv.pc /usr/lib/pkgconfig
-#checkinstall
-#rm /etc/ld.so.conf.d/opencv.conf
-#sh -c 'echo "/opt/ocv3.1.0/lib" > /etc/ld.so.conf.d/opencv.conf'
-#ldconfig
-#cd
+cp ./lib/python3/cv2.cpython-35m-x86_64-linux-gnu.so  /usr/local/lib/python3.5/dist-packages
+cp -p unix-install/opencv.pc /usr/lib/pkgconfig
+checkinstall
+rm /etc/ld.so.conf.d/opencv.conf
+sh -c 'echo "/opt/ocv3.1.0/lib" > /etc/ld.so.conf.d/opencv.conf'
+ldconfig
+cd
 
