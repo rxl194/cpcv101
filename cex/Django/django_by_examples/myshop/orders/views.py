bb757fb60ec9b32438051548fea6904c859f3401
rxl194 <Rendong_Lin@yahoo.com>
2016-11-05 00:55:00 +0000

    .

--- cex/Django/django_by_examples/myshop/orders/views.py
+++ cex/Django/django_by_examples/myshop/orders/views.py
@@ -4,6 +4,10 @@ from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
 def order_create(request):
     cart = Cart(request)
     if request.method == 'POST':
@@ -28,3 +32,10 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
