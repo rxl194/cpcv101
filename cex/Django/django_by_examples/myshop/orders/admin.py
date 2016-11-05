beef96fbcc88e6db2840db565a94c5532d70cc66
rxl194 <Rendong_Lin@yahoo.com>
2016-11-05 21:17:56 +0000

    .

--- cex/Django/django_by_examples/myshop/orders/admin.py
+++ cex/Django/django_by_examples/myshop/orders/admin.py
@@ -45,9 +45,17 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'email', 
-                    'address', 'postal_code', 'city', 'paid', 
-                    'created', 'updated']
+    list_display = ['id',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'address',
+                    'postal_code',
+                    'city',
+                    'paid',
+                    'created',
+                    'updated',
+                    order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
