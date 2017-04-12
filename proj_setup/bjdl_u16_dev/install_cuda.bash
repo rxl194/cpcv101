ef7af13ff14f6f64c6e12210974b4df3eac1633d
rxl194 <Rendong_Lin@yahoo.com>
2017-04-12 01:32:27 +0000

    .

--- proj_setup/bjdl_u16_dev/install_cuda.bash
+++ proj_setup/bjdl_u16_dev/install_cuda.bash
@@ -3,14 +3,15 @@
 mkdir -p ~/download
 cd ~/download/
 
+# steps 1)
 echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
 if ! dpkg-query -W cuda; then
   # The 16.04 installer works with 16.10.
   curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-  dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-  apt-get update
-  apt-get install cuda -y
+  sudo dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  sudo apt-get update
+  sudo apt-get install cuda -y
 fi
 
 # to verify
@@ -19,8 +20,15 @@ fi
 
 
 # install cuda-toolkit
-wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
-sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
-sudo apt-get update
-sudo apt-get install cuda  -y
+# SAME as above steps 1)
+#wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+#sudo dpkg -i cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64-deb
+#sudo apt-get update
+#sudo apt-get install cuda  -y
+
+# steps 2)
+sudo apt-get install -y nvidia-cuda-toolkit
+# to verify
+nvcc --version
+
+# TOTAL storage 9G
