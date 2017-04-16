8d7d76a3b80d5c6d8fa147d3ae65c4df369d38e1
rxl194 <Rendong_Lin@yahoo.com>
2017-04-15 21:57:57 -0700

    .

--- proj_setup/bjdl_u16_dev/cpcv_install_2.bash
+++ proj_setup/bjdl_u16_dev/cpcv_install_2.bash
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# // Install gcloud sdk
+# $ wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-151.0.1-linux-x86_64.tar.gz
+# $ tar zxvf google-cloud-sdk-151.0.1-linux-x86_64.tar.gz
+# $ mv google-cloud-sdk ~
+# $ ~/google-cloud-sdk/install.sh
+
+# $ gcloud init
+# $ gcloud auth login
+# $ gcloud auth application-default login
+
 # // INSTALL gcloud gcsfuse
-#  $ sudo gcloud init
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
